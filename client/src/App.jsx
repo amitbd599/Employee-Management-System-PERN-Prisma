dd8bc86a6217c0ffdd0ca0cbe6169f97cf41322c
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-03-20 00:16:53 +0600

    get-all-user page add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreateUserPage from "./pages/CreateUserPage";
 import UpdateUserPage from "./pages/UpdateUserPage";
+import GetUserPage from "./pages/GetUserPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route exact path='/' element={<HomePage />} />
         <Route exact path='/create-user' element={<CreateUserPage />} />
         <Route exact path='/update-user' element={<UpdateUserPage />} />
+        <Route exact path='/get-all-user' element={<GetUserPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/GetUserPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MasterLayout from "../layout/Masterlayout";
+import GetUser from "../components/GetUser";
+
+const GetUserPage = () => {
+  return (
+    <MasterLayout>
+      {/* GetUser */}
+      <GetUser />
+    </MasterLayout>
+  );
+};
+
+export default GetUserPage;
