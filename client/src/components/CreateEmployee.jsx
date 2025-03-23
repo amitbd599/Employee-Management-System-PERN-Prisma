f4682406a031606c00324db3cc1f5b75d22bf7a4
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-03-23 20:18:25 +0600

    CreateEmployee add

--- client/src/components/CreateEmployee.jsx
+++ client/src/components/CreateEmployee.jsx
@@ -1,6 +1,73 @@
-import { Select, Option } from "@material-tailwind/react";
+import EmployeeStore from "../../store/EmployeeStore";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ErrorToast, IsEmpty } from "../helper/helper";
+import SubmitButton from "./SubmitButton";
+import DepartmentStore from "../../store/DepartmentStore";
+import RoleStore from "../../store/RoleStore";
 
 const CreateEmployee = () => {
+  let { createEmployeeRequest, loadingRequest } = EmployeeStore();
+  let { getAllDepartmentsRequest, allDepartments } = DepartmentStore();
+  let { getAllRolesRequest, allRoles } = RoleStore();
+  let navigate = useNavigate();
+  let [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    salary: "",
+    departmentId: "",
+    roleId: "",
+  });
+
+  useEffect(() => {
+    (async () => {
+      await getAllDepartmentsRequest();
+      await getAllRolesRequest();
+    })();
+  }, [getAllDepartmentsRequest, getAllRolesRequest]);
+
+  let submitCreateEmployee = async () => {
+    let { firstName, lastName, email, phone, salary, departmentId, roleId } =
+      data;
+    if (IsEmpty(firstName)) {
+      ErrorToast("First Name is required. ");
+      return;
+    } else if (IsEmpty(lastName)) {
+      ErrorToast("Last Name is required. ");
+      return;
+    } else if (IsEmpty(email)) {
+      ErrorToast("Email is required. ");
+      return;
+    } else if (IsEmpty(phone)) {
+      ErrorToast("Phone is required. ");
+      return;
+    } else if (IsEmpty(salary)) {
+      ErrorToast("Salary is required. ");
+      return;
+    } else if (IsEmpty(departmentId)) {
+      ErrorToast("Department is required. ");
+      return;
+    } else if (IsEmpty(roleId)) {
+      ErrorToast("Role is required. ");
+      return;
+    } else {
+      let res = await createEmployeeRequest({
+        firstName,
+        lastName,
+        email,
+        phone,
+        salary: parseFloat(salary),
+        departmentId,
+        roleId,
+      });
+      if (res) {
+        navigate("/get-all-employee/1");
+      }
+    }
+  };
+
   return (
     <section>
       <>
@@ -10,13 +77,16 @@ const CreateEmployee = () => {
             Create a new employee
           </h1>
 
-          <form>
+          <div>
             <div className='flex gap-[20px] mb-4'>
               <div className='w-full grid gap-1'>
                 <label htmlFor='firstName' className='text-gray-600'>
                   First Name:
                 </label>
                 <input
+                  onChange={(e) =>
+                    setData({ ...data, firstName: e.target.value })
+                  }
                   id='firstName'
                   type='text'
                   className='border p-2 rounded w-full h-[40px] focus:outline-none text-gray-600'
@@ -27,6 +97,9 @@ const CreateEmployee = () => {
                   Last Name:
                 </label>
                 <input
+                  onChange={(e) =>
+                    setData({ ...data, lastName: e.target.value })
+                  }
                   id='lastName'
                   type='text'
                   className='border p-2 rounded w-full h-[40px] focus:outline-none text-gray-600'
@@ -39,6 +112,7 @@ const CreateEmployee = () => {
                   Email:
                 </label>
                 <input
+                  onChange={(e) => setData({ ...data, email: e.target.value })}
                   id='email'
                   type='email'
                   className='border p-2 rounded w-full h-[40px] focus:outline-none text-gray-600'
@@ -49,6 +123,7 @@ const CreateEmployee = () => {
                   Phone:
                 </label>
                 <input
+                  onChange={(e) => setData({ ...data, phone: e.target.value })}
                   id='phone'
                   type='text'
                   className='border p-2 rounded w-full h-[40px] focus:outline-none text-gray-600'
@@ -61,6 +136,7 @@ const CreateEmployee = () => {
                   Salary:
                 </label>
                 <input
+                  onChange={(e) => setData({ ...data, salary: e.target.value })}
                   id='salary'
                   type='number'
                   className='border p-2 rounded w-full h-[40px] focus:outline-none text-gray-600'
@@ -71,13 +147,20 @@ const CreateEmployee = () => {
                   <label htmlFor='department' className='text-gray-600'>
                     Department:
                   </label>
-                  <Select label='Select Version'>
-                    <Option>Material Tailwind HTML</Option>
-                    <Option>Material Tailwind React</Option>
-                    <Option>Material Tailwind Vue</Option>
-                    <Option>Material Tailwind Angular</Option>
-                    <Option>Material Tailwind Svelte</Option>
-                  </Select>
+                  <select
+                    className='border p-2 rounded w-full h-[40px] focus:outline-none cursor-pointer text-gray-600'
+                    value={data?.departmentId}
+                    onChange={(e) =>
+                      setData({ ...data, departmentId: e.target.value })
+                    }
+                  >
+                    <option value=''>Select department</option>
+                    {allDepartments?.map((item, index) => (
+                      <option value={item?.id} key={index}>
+                        {item?.name}
+                      </option>
+                    ))}
+                  </select>
                 </div>
               </div>
               <div className=' w-full'>
@@ -85,24 +168,31 @@ const CreateEmployee = () => {
                   <label htmlFor='Role' className='text-gray-600'>
                     Role:
                   </label>
-                  <Select label='Select Version'>
-                    <Option>Material Tailwind HTML</Option>
-                    <Option>Material Tailwind React</Option>
-                    <Option>Material Tailwind Vue</Option>
-                    <Option>Material Tailwind Angular</Option>
-                    <Option>Material Tailwind Svelte</Option>
-                  </Select>
+                  <select
+                    className='border p-2 rounded w-full h-[40px] focus:outline-none cursor-pointer text-gray-600'
+                    value={data?.roleId}
+                    onChange={(e) =>
+                      setData({ ...data, roleId: e.target.value })
+                    }
+                  >
+                    <option value=''>Select role</option>
+                    {allRoles?.map((item, index) => (
+                      <option value={item?.id} key={index}>
+                        {item?.name}
+                      </option>
+                    ))}
+                  </select>
                 </div>
               </div>
             </div>
 
-            <button
-              type='button'
-              className='px-[26px] py-[12px] rounded bg-[#487FFF] text-white hover:bg-blue-600 focus:outline-none transition'
-            >
-              Create Employee
-            </button>
-          </form>
+            <SubmitButton
+              text='Create new employee'
+              type='submit'
+              submitFun={submitCreateEmployee}
+              isSubmitting={loadingRequest}
+            />
+          </div>
         </div>
       </>
     </section>
